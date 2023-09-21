5cd73c479b72a7115cc9a7f91eee321c7f160fc6
Christophe Muracciole <64611874+muriatic@users.noreply.github.com>
2023-09-21 18:26:01 -0400

    Added ToLower function for vectors of strings

--- src/vector_functions.h
+++ src/vector_functions.h
@@ -2,6 +2,7 @@
 #define VECTORFUNCTIONS_H
 
 #include <vector>
+#include <string>
 
 template <typename T> int IndexOf(std::vector<T> vect, T value)
 {
@@ -16,6 +17,16 @@ template <typename T> int IndexOf(std::vector<T> vect, T value)
 	int idx = it - vect.begin();
 
 	return idx;
+}
+
+void ToLower(std::vector<std::string>& vect)
+{
+	for (std::string str : vect)
+	{
+		// make lowercase only
+		std::transform(str.begin(), str.end(), str.begin(),
+			[](unsigned char c) { return std::tolower(c); });
+	}
 }
 
 #endif
