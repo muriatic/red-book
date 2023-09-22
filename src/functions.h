7f8b786ad71e11c712e88191de7de281b6f9d833
Christophe Muracciole <64611874+muriatic@users.noreply.github.com>
2023-09-21 23:45:00 -0400

    Moved over functions
    added ToUpper and RemoveSpaces

--- src/functions.h
+++ src/functions.h
@@ -20,4 +20,31 @@ std::vector<std::string> split(const std::string& s, char delim) {
 	return result;
 }
 
+std::string ToLower(std::string str)
+{
+	// make lowercase only
+	std::transform(str.begin(), str.end(), str.begin(),
+		[](unsigned char c) { return std::tolower(c); });
+
+	return str;
+}
+
+std::string ToUpper(std::string str)
+{
+	// make uppercase only
+	std::transform(str.begin(), str.end(), str.begin(),
+		[](unsigned char c) { return std::toupper(c); });
+
+	return str;
+}
+
+// removes all whitespace
+std::string RemoveSpaces(std::string str)
+{
+	// remove all spaces
+	str.erase(remove(str.begin(), str.end(), ' '), str.end());
+
+	return str;
+}
+
 #endif
