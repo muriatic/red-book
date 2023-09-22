f7a3b38ab869e937b55d3b79648273dbb6f544bf
Christophe Muracciole <64611874+muriatic@users.noreply.github.com>
2023-09-22 15:08:40 -0400

    Added Strip(); Capitalize(); CapitalizeTitle()

--- src/functions.h
+++ src/functions.h
@@ -47,4 +47,49 @@ std::string RemoveSpaces(std::string str)
 	return str;
 }
 
+// removes leading a trailing whitespace
+std::string Strip(std::string str)
+{
+	// remove leading and trailing whitespace
+	return std::regex_replace(str, std::regex("^ +| +$|( ) +"), "$1");
+}
+
+// capitalizes the first letter
+std::string Capitalize(std::string str)
+{
+	str[0] = toupper(str[0]);
+	return str;
+}
+
+std::string CapitalizeTitle(std::string str)
+{
+	std::vector<std::string> nonCapitalizedWords {"to", "a", "at", "the", "on", "of", "in", "and", "for", "or", "an", "as"};
+
+	std::vector<std::string> words = split(str, ' ');
+
+	std::string title;
+
+	for (int i =0; i < words.size(); i++)
+	{
+		// first word is always capitalized
+		if (i == 0)
+		{
+			title += Capitalize(words[i]);
+			continue;
+		}
+
+		// its a prep, etc...
+		int IDX = IndexOf(nonCapitalizedWords, ToLower(words[i]));
+		if (IDX != -1)
+		{
+			title += " " + nonCapitalizedWords[IDX];
+			continue;
+		}
+
+		title += " " + Capitalize(words[i]);
+	}
+
+	return title;
+}
+
 #endif
