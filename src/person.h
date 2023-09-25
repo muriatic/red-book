de2fc6f32a7848e462df5d293235a896947e8594
Christophe Muracciole <64611874+muriatic@users.noreply.github.com>
2023-09-24 23:40:55 -0400

    Added save function

--- src/person.h
+++ src/person.h
@@ -5,6 +5,7 @@
 #include <vector>
 #include <sstream>
 #include <regex>
+#include <fstream>
 
 #include "functions.h"
 
@@ -26,10 +27,69 @@ class Person
 		askMeAbout = split(row.getValue("Ask me about... (Professional Information)"), '/');
 		currentPositions = split(row.getValue("Current AKPsi Position(s)"), '/');
 	}
+
+	std::string CreateRow(std::string line)
+	{
+		std::string row;
+		row += line;
+		return row;
+	}
+	std::string CreateRow(std::vector<std::string> line)
+	{
+		std::string row;
+		row += "<";
+		row += Join(line, ",");
+		row += ">";
+		return row;
+	}
+	std::string CreateRow(std::vector<std::vector<std::string>> line)
+	{
+		std::string row;
+		row += "<";
+		for (std::vector<std::string> entry : line)
+		{
+			row += "<";
+			row += Join(entry, ",");
+			row += ">";
+		}
+		row += ">";
+		return row;
+	}
+
 public:
 	Person(Row row)
 	{
 		parseRow(row);
+	}
+
+	void Save(std::string saveName = "")
+	{
+		if (saveName == "")
+		{
+			saveName = "brothers/" + name + ".brother";
+		}
+
+		std::vector<std::string> rows;
+		rows.push_back(CreateRow(name));
+		rows.push_back(CreateRow(majors));
+		rows.push_back(CreateRow(minors));
+		rows.push_back(CreateRow(gradDate));
+		rows.push_back(CreateRow(pronouns));
+		rows.push_back(CreateRow(phoneNumber));
+		rows.push_back(CreateRow(email));
+		rows.push_back(CreateRow(linkedin));
+		rows.push_back(CreateRow(professionalInvolvement));
+		rows.push_back(CreateRow(askMeAbout));
+		rows.push_back(CreateRow(currentPositions));
+		rows.push_back(CreateRow(gradProgram));
+		rows.push_back(CreateRow(resumeFile));
+		rows.push_back(CreateRow(imageFile));
+		rows.push_back(CreateRow(resumeInfo));
+		
+		std::string contents = Join(rows, "\n");
+
+		std::fstream file(saveName, std::ios::out);
+		file << contents;
 	}
 
 	std::string name;
