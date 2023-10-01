3224e3f476041273f392c4e1fac6d637ad5b26fe
Christophe Muracciole <64611874+muriatic@users.noreply.github.com>
2023-10-01 01:06:38 -0400

    Update person.h
    Added constructor overload for when youre reading a file
    
    changed how CreateRow() works so it's easier for loading
    
    Added parseLines, parseMap, parseVector which deal with loading it from a .brother file

--- src/person.h
+++ src/person.h
@@ -29,6 +29,84 @@ class Person
 		currentPositions = split(row.getValue("Current AKPsi Position(s)"), '/');
 	}
 
+	void parseVector(std::string line, std::vector<std::string> &vect)
+	{
+		//
+		if (!has_prefix(line, "<") && !has_suffix(line, ">")) {
+			vect = split(line, ',');
+		}
+		else {
+			std::string tempLine;
+			// removes < > at beginning and end
+			tempLine = line.substr(1, line.size() - 2);
+			vect = split(tempLine, ',');
+		}
+	}
+	void parseMap(std::string line, std::vector<std::map<std::string, std::string>> &mapVector)
+	{
+		// remove { & } at beginning and end 
+		line = line.substr(1, line.size() - 2);
+
+		std::vector<std::string> entries = split(line, "}{");
+
+		for (int i = 0; i < entries.size(); i++)
+		{
+			std::vector<std::string> keySets = split(entries[i], ';');
+
+			std::map<std::string, std::string> tempMap;
+
+			for (std::string keySet : keySets)
+			{
+				//std::vector<std::string> keyANDset = split(keySet, ':');
+
+				int firstColonIDX = IndexOf(keySet, ":");
+				std::string key = keySet.substr(0, firstColonIDX);
+				key = Strip(key, '"');
+
+				std::string value = keySet.substr(firstColonIDX+1, keySet.size() - firstColonIDX);
+				value = Strip(value, '"');
+
+
+				tempMap.insert({ key, value });
+			}
+
+			mapVector.push_back(tempMap);
+		}
+	}
+	void parseLines(std::vector<std::string> lines)
+	{
+		// all files should be 16 lines (if they have no work experience it'll be 15)
+		if (lines.size() == 15)
+		{
+			lines.push_back("");
+		}
+		if (lines.size() != 16)
+		{
+			std::cerr << "a non-16 line vector was provided. " << lines.size() << " lines for " << lines[0] << std::endl;
+			exit(EXIT_FAILURE);
+		}
+		name = lines[0];
+
+		parseVector(lines[1], majors);
+		parseVector(lines[2], minors);
+		gradDate = lines[3];
+		pronouns = lines[4];
+		phoneNumber = lines[5];
+		email = lines[6];
+		linkedin = lines[7];
+		parseVector(lines[8], professionalInvolvement);
+		parseVector(lines[9], askMeAbout);
+		parseVector(lines[10], currentPositions);
+		gradProgram = lines[11];
+		resumeFile = lines[12];
+		imageFile = lines[13];
+		// i dont think im gonna need the resume info line so im going to skip over it
+		if (Strip(lines[15]) != "")
+		{
+			parseMap(lines[15], parsedResumeInfo);
+		}
+	}
+
 	std::string CreateRow(std::string line)
 	{
 		std::string row;
@@ -38,50 +116,47 @@ class Person
 	std::string CreateRow(std::vector<std::string> line)
 	{
 		std::string row;
-		row += "<";
 		row += Join(line, ",");
-		row += ">";
 		return row;
 	}
 	std::string CreateRow(std::vector<std::vector<std::string>> line)
 	{
 		std::string row;
-		row += "<";
 		for (std::vector<std::string> entry : line)
 		{
 			row += "<";
 			row += Join(entry, ",");
 			row += ">";
 		}
-		row += ">";
 		return row;
 	}
 	std::string CreateRow(std::vector<std::map<std::string, std::string>> line)
 	{
 		std::string row;
-		row += "<";
 		for (std::map<std::string, std::string> entry : line)
 		{
 			row += "{";
 			int count = entry.size();
 			for (auto m : entry)
 			{
+				row += "\"";
 				row += m.first;
+				row += "\"";
 				row += ":";
+				row += "\"";
 				row += m.second;
+				row += "\"";
 
 				// this ensures that commas are only in between entries
 				if (count != 1)
 				{
-					row += ",";
+					row += ";";
 				}
 
 				count--;
 			}
 			row += "}";
 		}
-
-		row += ">";
 
 		return row;
 	}
@@ -90,6 +165,10 @@ public:
 	Person(Row row)
 	{
 		parseRow(row);
+	}
+	Person(std::vector<std::string> lines)
+	{
+		parseLines(lines);
 	}
 
 	void Save(std::string saveName = "")
